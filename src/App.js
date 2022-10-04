8d645a11b362b46410a4751a949a8e9d53ec1ddd
Patel Bhavya M <bhavyapatel4012@gmail.com>
2022-10-04 22:19:12 +0530

    Final Call End Project

--- src/App.js
+++ src/App.js
@@ -14,16 +14,19 @@ function App() {
         <div className="flex justify-center text-violet-600">
           Team - MeDoV
         </div>
+        
         </p>
       </div>
 
       <div className="flex justify-center m-5 text-violet-500 font-mono text-3xl mb-0">
         <p>Download Our Prototype APK Form Given Link Below</p>
       </div>
-
       <div className='m-1 flex justify-center'>
-            <a href="my-app\public\app\MedoV.apk" download className="inline-flex items-center py-3 px-6 mt-10 text-xs sm:text-sm font-medium text-white  bg-violet-700 rounded-md border border-gray-200  hover:bg-violet-500 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-white dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Download APK</a>
+            <a  href="https://drive.google.com/file/d/1x9cOBbCAkbS4BVaHN3YsaDMqjsJslgET/view?usp=sharing" download className="inline-flex items-center py-3 px-6 mt-10 text-xs sm:text-sm font-medium text-white  bg-violet-700 rounded-md border border-gray-200  hover:bg-violet-500 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-white dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Download APK</a>
       </div>
+        <div className="flex justify-center text-violet-600 text-4xl m-6">
+          Thank - You 
+        </div>
   </div>
   </>
   );
