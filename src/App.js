24c7619d72dd62aaa50f1803af23c179ad5efcd0
Patel Bhavya M <bhavyapatel4012@gmail.com>
2022-10-04 21:10:19 +0530

    Final Call next Update Download button

--- src/App.js
+++ src/App.js
@@ -15,7 +15,14 @@ function App() {
           Team - MeDoV
         </div>
         </p>
-        
+      </div>
+
+      <div className="flex justify-center m-5 text-violet-500 font-mono text-3xl mb-0">
+        <p>Download Our Prototype APK Form Given Link Below</p>
+      </div>
+
+      <div className='m-1 flex justify-center'>
+            <a href="/" download className="inline-flex items-center py-3 px-6 mt-10 text-xs sm:text-sm font-medium text-white  bg-violet-700 rounded-md border border-gray-200  hover:bg-violet-500 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-white dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Download APK</a>
       </div>
   </div>
   </>
