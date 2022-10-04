573fb6c8f8f53beca8eae9ff43f8576e53032c87
Patel Bhavya M <bhavyapatel4012@gmail.com>
2022-10-04 20:46:57 +0530

    Till Header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './index.css';
+import svg from './icons/Landing.svg'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  <>
+  <div style={{backgroundImage : `url(${svg})` }} className='w-full m-0 p-0 relative h-screen'>
+      <div className='flex justify-center m-10'>
+        <p className="text-violet-500 font-mono text-5xl font-bold">
+        NEXT BIG THING - HACKATHON
+        <div className="flex justify-center text-violet-700">
+          Presented by google cloud
+        </div>
+        <div className="flex justify-center text-violet-600">
+          Team - MeDoV
+        </div>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        
+      </div>
+  </div>
+  </>
   );
 }
 
