3069f866a838f77a5ddffa1cbba9ae7b15b3d0c7
Patel Bhavya M <bhavyapatel4012@gmail.com>
2022-10-06 20:50:28 +0530

    Final Link Submit no error at all

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import svg from './icons/Landing.svg'
 function App() {
   return (
   <>
-  <div style={{backgroundImage : `url(${svg})` }} className='w-full m-0 p-0 relative h-screen'>
+  <div style={{backgroundImage : `url(${svg})` }} className='first w-full m-0 p-0 relative h-screen'>
       <div className='flex justify-center m-10'>
         <p className="text-violet-500 font-mono text-5xl font-bold">
         NEXT BIG THING - HACKATHON
@@ -12,19 +12,18 @@ function App() {
           Presented by google cloud
         </div>
         <div className="flex justify-center text-violet-600">
-          Team - MeDoV
+          Team - TechnoV
         </div>
-        
         </p>
       </div>
 
       <div className="flex justify-center m-5 text-violet-500 font-mono text-3xl mb-0">
-        <p>Download Our Prototype APK Form Given Link Below</p>
+        <p>Download Our Prototype APK From Given Link Below</p>
       </div>
       <div className='m-1 flex justify-center'>
-            <a  href="https://drive.google.com/file/d/1x9cOBbCAkbS4BVaHN3YsaDMqjsJslgET/view?usp=sharing" download className="inline-flex items-center py-3 px-6 mt-10 text-xs sm:text-sm font-medium text-white  bg-violet-700 rounded-md border border-gray-200  hover:bg-violet-500 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-white dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Download APK</a>
+            <a  href="https://drive.google.com/file/d/1ZCUKT6Act0mr10_xaOf5Ct5Xcp6Bn4XH/view?usp=sharing" download className="inline-flex items-center py-3 px-6 mt-10 text-xs sm:text-sm font-medium text-white  bg-violet-700 rounded-md border border-gray-200  hover:bg-violet-500 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-white dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Download APK</a>
       </div>
-        <div className="flex justify-center text-violet-600 text-4xl m-6">
+        <div className="flex justify-center text-violet-600 text-4xl m-7">
           Thank - You 
         </div>
   </div>
